f3840a3212d8aa940e0084c52e74c4ad3e4ea79f
Purring Bytes <purringbytes@gmail.com>
2025-06-11 14:53:00 +0200

    all physics structs moved now.

--- src/client/include/MagixStructs.h
+++ src/client/include/MagixStructs.h
@@ -161,6 +161,208 @@ struct WorldCritter
 	}
 };
 
+/// <summary>
+/// All physics structcs (but some may get removed such as portal / wall)
+/// </summary>
+
+struct Collision
+{
+	SceneNode* mNode;
+	Real range;
+	Vector3 offset;
+	Vector3 force;
+	vector<unsigned short>::type hitID;
+	vector<unsigned short>::type critterHitID;
+	short hp;
+	unsigned char alliance;
+	bool hitAlly;
+	Collision()
+	{
+		mNode = 0;
+		range = 0;
+		offset = Vector3::ZERO;
+		force = Vector3::ZERO;
+		hitID.clear();
+		critterHitID.clear();
+		hp = 0;
+		alliance = 0;
+		hitAlly = false;
+	}
+	const Vector3 getPivot()
+	{
+		//if(!mCollOwnerNode[iID])return Vector3::ZERO;
+		//return mCollOwnerNode[iID]->getPosition()+mCollOwnerNode[iID]->getOrientation()*collOffset[iID];
+		if (!mNode)return Vector3::ZERO;
+		return mNode->getPosition() + mNode->getOrientation() * offset;
+	}
+	const Vector3 getForce(bool fromPivot = false, const Vector3& targetPosition = Vector3::ZERO)
+	{
+		if (fromPivot)
+		{
+			const Vector3 tVect = targetPosition - getPivot();
+			const Quaternion tQuat = force.getRotationTo(tVect);
+			return tQuat * force;
+		}
+		return force;
+	}
+	bool collides(const AxisAlignedBox& target)
+	{
+		if (!mNode)return false;
+		//if(mCollEnt[iID]->getWorldBoundingSphere().intersects(target))return true;
+		if (Sphere(getPivot(), range).intersects(target))return true;
+		return false;
+	}
+	bool hasHitID(const unsigned short& id)
+	{
+		for (int i = 0; i < (int)hitID.size(); i++)
+		{
+			if (hitID[i] == id)return true;
+		}
+		return false;
+	}
+	bool hasCritterHitID(const unsigned short& id)
+	{
+		for (int i = 0; i < (int)critterHitID.size(); i++)
+		{
+			if (critterHitID[i] == id)return true;
+		}
+		return false;
+	}
+};
+
+struct Wall
+{
+	bool isActive;
+	bool isSphere;
+	Vector3 center;
+	Vector3 range;
+	bool isInside;
+	Wall()
+	{
+		isActive = false;
+		isSphere = false;
+		center = Vector3::ZERO;
+		range = Vector3::ZERO;
+		isInside = false;
+	}
+	bool collides(const Vector3& target)
+	{
+		if (!isActive)return false;
+
+		if (isSphere)
+		{
+			//Fix y coordinate to that of target's to create a Cylinder wall
+			const Sphere tSphere(Vector3(center.x, target.y, center.z), range.x);
+			if (isInside)return(tSphere.intersects(target));
+			else return(!tSphere.intersects(target));
+		}
+		else
+		{
+			//Fix y coordinate to that of target's to create an infinitely tall wall
+			const AxisAlignedBox tBox(Vector3(center.x - range.x / 2, target.y, center.z - range.z / 2),
+				Vector3(center.x + range.x / 2, target.y, center.z + range.z / 2));
+			if (isInside)return(tBox.intersects(target));
+			else return(!tBox.intersects(target));
+		}
+		return false;
+	}
+};
+
+struct Portal
+{
+	SceneNode* mNode;
+	Entity* mEnt;
+	String dest;
+	Portal()
+	{
+		mNode = 0;
+		mEnt = 0;
+		dest = "";
+	}
+	bool collides(const AxisAlignedBox& target)
+	{
+		if (!mEnt || dest == "")return false;
+		if (mEnt->getWorldBoundingSphere().intersects(target))return true; // getWorldBoundingSphere doesn't work properly
+		return false;
+	}
+	void disable()
+	{
+		dest = "";
+	}
+};
+
+struct Gate
+{
+	SceneNode* mNode;
+	Entity* mEnt;
+	String dest;
+	Vector3 destVect;
+	bool hasVectY;
+	Gate()
+	{
+		mNode = 0;
+		mEnt = 0;
+		dest = "";
+		destVect = Vector3::ZERO;
+		hasVectY = false;
+	}
+	bool collides(const AxisAlignedBox& target)
+	{
+		if (!mEnt || dest == "")return false;
+		if (mEnt->getWorldBoundingSphere().intersects(target))return true; // getWorldBoundingSphere doesn't work properly
+		return false;
+	}
+	void disable()
+	{
+		dest = "";
+	}
+};
+
+struct WaterBox
+{
+	bool isActive;
+	Vector3 center;
+	Vector2 scale;
+	bool isSolid;
+	WaterBox()
+	{
+		isActive = false;
+		center = Vector3::ZERO;
+		scale = Vector2::ZERO;
+		isSolid = false;
+	}
+	bool collides(const Vector3& target)
+	{
+		if (!isActive)return false;
+		if (target.y > center.y)return false;
+		//Create infinite depth box
+		const AxisAlignedBox tBox(Vector3(center.x - scale.x / 2, target.y, center.z - scale.y / 2),
+			Vector3(center.x + scale.x / 2, target.y, center.z + scale.y / 2));
+		if (tBox.intersects(target))return true;
+		return false;
+	}
+};
+
+struct CollBox
+{
+	//Collbox takes center like a typical mesh, i.e. midpoint of "feet"
+	Vector3 center;
+	Vector3 range;
+	CollBox()
+	{
+		center = Vector3::ZERO;
+		range = Vector3::ZERO;
+	}
+	bool collides(const Vector3& target, const Real& headHeight)
+	{
+		//Extend below by unit height to detect head collision
+		const AxisAlignedBox tBox(Vector3(center.x - range.x / 2, center.y - headHeight, center.z - range.z / 2),
+			Vector3(center.x + range.x / 2, center.y + range.y, center.z + range.z / 2));
+		if (tBox.intersects(target))return true;
+		return false;
+	}
+};
+
 struct CollSphere
 {
 	Vector3 center;
